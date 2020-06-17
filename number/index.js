93e24043f5697e272516facfd9c02b831ae39647
Vicky Lin <jiajun_work@126.com>
2020-06-17 11:50:15 +0800

    finish loop homework

---
+++ number/index.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 20; i++) {
+    if (i%2==0) {
+        document.write(i+" is even number.")
+    }else{
+        document.write(i+" is odd number.")
+    }
+    document.write("<br>")
+}
